651e2b5d069fa844f2b1860b17412526ba488faf
n <kfares29@gmail.com>
2016-01-05 22:55:56 +0200

    Editing open-uri_allow_redirect to open_uri_allow_redirect

--- lib/askwiki.rb
+++ lib/askwiki.rb
@@ -1,4 +1,4 @@
-require 'open-uri_allow_redirect'
+require 'open_uri_allow_redirect'
 require 'json'
 
 class Askwiki
@@ -6,19 +6,21 @@ class Askwiki
 
 
   def initialize( lang='en' )
-  @language = lang
+    @language = lang
   end
 
   def ask query
-  query_encoded = URI::encode(query)
-  request_url = "http://#{@language}.wikipedia.org/w/api.php?action=parse&page=#{query_encoded}&format=json&prop=text&section=0&redirects"
+    query_encoded = URI::encode(query)
+    request_url = "http://#{@language}.wikipedia.org/w/api.php?action=parse&page=#{query_encoded}&format=json&prop=text&section=0&redirects"
     text = ''
-  open(request_url) do |file|
-  text = JSON.parse(file.read())['parse']['text'].first[1]
-  .gsub(/<\/?[^>]+>/, '') # strip tags
-  .gsub(/[[:space:]]+/, ' ') # strip whitespace
-  .gsub(/&#[0-9]+;/,'') # strip encoded
-  .gsub(/\[[0-9]+\]/,'') # strip referencing
+    OpenURI.allow_redirect do
+      open(request_url) do |file|
+        text = JSON.parse(file.read())['parse']['text'].first[1]
+          .gsub(/<\/?[^>]+>/, '') # strip tags
+          .gsub(/[[:space:]]+/, ' ') # strip whitespace
+          .gsub(/&#[0-9]+;/,'') # strip encoded
+          .gsub(/\[[0-9]+\]/,'') # strip referencing
+      end
     end
     text
   end
